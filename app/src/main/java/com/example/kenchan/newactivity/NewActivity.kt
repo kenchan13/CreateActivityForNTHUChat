e288a749efb4cb9863e28392f7c5788f4e40e91e
Ken Chan <kenchan13@gmail.com>
2018-10-26 00:19:38 +0800

    add Map Picker

--- app/src/main/java/com/example/kenchan/newactivity/NewActivity.kt
+++ app/src/main/java/com/example/kenchan/newactivity/NewActivity.kt
@@ -1,9 +1,16 @@
 package com.example.kenchan.newactivity
 
+import android.app.Activity
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.support.v7.app.AlertDialog
+import android.view.View
 import android.widget.*
 import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.database.DataSnapshot
@@ -11,10 +18,15 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_new.*
 import java.text.SimpleDateFormat
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.places.Places
+import com.google.android.gms.location.places.ui.PlacePicker
 import java.util.*
 
 
 class NewActivity : AppCompatActivity() {
+    private val PLACE_PICKER_REQUEST = 1
+    private var mGoogleApiClient: GoogleApiClient? = null
 
     var database = FirebaseDatabase.getInstance()
     var myRef = database.getReference("message")
@@ -54,6 +66,7 @@ class NewActivity : AppCompatActivity() {
             dpd.show()
         }
 
+
         endDateUI.setOnClickListener {
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
@@ -91,12 +104,23 @@ class NewActivity : AppCompatActivity() {
             TimePickerDialog(this, timeSetListenner, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
         }
 
-
-
         buttonSendUI = findViewById(R.id.buttonSend)
         buttonSendUI.setOnClickListener {
             sendSchedule()
         }
+
+        //Initialise GoogleApiClient
+        buildGoogleApiClient()
+
+        //OnClickListener
+        tvPlace.setOnClickListener(View.OnClickListener {
+            if (!checkGPSEnabled()) {
+                return@OnClickListener
+            }
+            val builder = PlacePicker.IntentBuilder()
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST)
+        })
+
     }
 
     fun sendSchedule() {
@@ -126,5 +150,45 @@ class NewActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, "Schedule Add Success!", Toast.LENGTH_LONG).show()
         }
 
+    }
+
+    private fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, null)
+                .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == PLACE_PICKER_REQUEST && resultCode == Activity.RESULT_OK) {
+            val place = PlacePicker.getPlace(data, this)
+            val toastMsg = String.format("Place: %s", place.name)
+            tvPlace.text = place!!.name.toString().plus("\n".plus(place!!.address).plus("\n".plus(place!!.phoneNumber)))
+            Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkGPSEnabled(): Boolean {
+        if (!isLocationEnabled())
+            showAlert()
+        return isLocationEnabled()
+    }
+
+    private fun showAlert() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Enable Location")
+                .setMessage("Locations Settings is set to 'Off'.\nEnable Location to use this app")
+                .setPositiveButton("Location Settings") { paramDialogInterface, paramInt ->
+                    val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(myIntent)
+                }
+                .setNegativeButton("Cancel") { paramDialogInterface, paramInt -> }
+        dialog.show()
     }
 }
